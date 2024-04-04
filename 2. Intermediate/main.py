4ea1ee0ce59e798aad695cd86ec00a40d882d313
Eric Roby <ericroby@Erics-MacBook-Pro.local>
2024-04-04 10:53:50 -0400

    Updated course

--- 2. Intermediate/main.py
+++ 2. Intermediate/main.py
@@ -1,21 +1,22 @@
 from openai import OpenAI
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Request, WebSocket
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from mangum import Mangum
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 openai = OpenAI(
-    api_key='YOUR API KEY GOES HERE!'
+    api_key = os.getenv('OPENAI_API_SECRET_KEY')
 )
 
 app = FastAPI()
-handler = Mangum(app)
 
 templates = Jinja2Templates(directory="templates")
 
 chat_responses = []
-
 
 @app.get("/", response_class=HTMLResponse)
 async def chat_page(request: Request):
@@ -23,11 +24,39 @@ async def chat_page(request: Request):
 
 
 chat_log = [{'role': 'system',
-             'content': 'You are a Python tutor AI, completely dedicated to teach users how to learn \
-                        Python from scratch. Please provide clear instructions on Python concepts, \
-                        best practices and syntax. Help create a path of learning for users to be able \
-                        to create real life, production ready python applications.'
+             'content': 'You tell jokes.'
              }]
+
+
+@app.websocket("/ws")
+async def chat(websocket: WebSocket):
+
+    await websocket.accept()
+
+    while True:
+        user_input = await websocket.receive_text()
+        chat_log.append({'role': 'user', 'content': user_input})
+        chat_responses.append(user_input)
+
+        try:
+            response = openai.chat.completions.create(
+                model='gpt-4',
+                messages=chat_log,
+                temperature=0.6,
+                stream=True
+            )
+
+            ai_response = ''
+
+            for chunk in response:
+                if chunk.choices[0].delta.content is not None:
+                    ai_response += chunk.choices[0].delta.content
+                    await websocket.send_text(chunk.choices[0].delta.content)
+            chat_responses.append(ai_response)
+
+        except Exception as e:
+            await websocket.send_text(f'Error: {str(e)}')
+            break
 
 
 @app.post("/", response_class=HTMLResponse)
@@ -37,7 +66,7 @@ async def chat(request: Request, user_input: Annotated[str, Form()]):
     chat_responses.append(user_input)
 
     response = openai.chat.completions.create(
-        model='gpt-3.5-turbo',
+        model='gpt-4',
         messages=chat_log,
         temperature=0.6
     )
@@ -60,7 +89,7 @@ async def create_image(request: Request, user_input: Annotated[str, Form()]):
     response = openai.images.generate(
         prompt=user_input,
         n=1,
-        size="512x512"
+        size="256x256"
     )
 
     image_url = response.data[0].url
